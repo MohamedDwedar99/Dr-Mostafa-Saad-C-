54242d7241f6d68eef922adf0d2155aaf621cf47
Mohamed Dwedar <MohamedDwedar2699@gmail.com>
2024-03-15 03:55:33 +0200

    Recursive function

---
+++ Mastering 4 critical SKILLS using C++ 17/12-Recursive Function/Eazy/07-Array Accumulation.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+
+void accumulate_arr(int arr[],int len)
+{
+    int max = arr[len-1];
+    if (len == 1)
+    {
+        return;
+    }
+    else
+    {
+        accumulate_arr(arr,len-1);
+        arr[len-1] += arr[len-2];
+    }
+}
+
+int main(void)
+{
+    int arr[100];
+    int len;
+    cin >> len;
+    for (int i = 0; i < len; i++)
+    {
+        cin >> arr[i];
+    }
+    accumulate_arr(arr,len);
+    for (int i = 0; i < len; i++)
+    {
+        cout << arr[i]<< " ";
+    }
+    cout << endl;
+    return 0;
+}
