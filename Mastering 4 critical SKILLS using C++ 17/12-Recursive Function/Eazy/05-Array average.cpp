4209d79c3ba11250a2a8b6ee00d2844a5dd571b5
Mohamed Dwedar <MohamedDwedar2699@gmail.com>
2024-03-15 03:38:01 +0200

    Recursive function

---
+++ Mastering 4 critical SKILLS using C++ 17/12-Recursive Function/Eazy/05-Array average.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+double average(int arr[],int len)
+{
+    int max = arr[len-1];
+    if (len == 1)
+    {
+        return arr[0];
+    }
+    else
+    {
+        return (arr[len-1] + average(arr,len-1)*(len-1))/len;
+    }
+}
+
+int main(void)
+{
+    int arr[100];
+    int len;
+    cin >> len;
+    for (int i = 0; i < len; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << average(arr,len);
+    return 0;
+}
