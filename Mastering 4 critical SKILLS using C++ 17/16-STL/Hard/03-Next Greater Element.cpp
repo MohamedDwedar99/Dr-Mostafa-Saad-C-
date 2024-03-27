a4dbdd295d085e361c6d7c9be12db34e9c2238d0
Mohamed Dwedar <MohamedDwedar2699@gmail.com>
2024-03-27 23:52:36 +0200

    STD

--- Mastering 4 critical SKILLS using C++ 17/16-STL/Hard/03-Next Greater Element.cpp
+++ Mastering 4 critical SKILLS using C++ 17/16-STL/Hard/03-Next Greater Element.cpp
@@ -17,6 +17,10 @@ vector<int> next_greater_num(vector<int>& v) {
     }
     return result;
 }
+vector<int> next_greater_num(vector<int>& v) {
+    vector<int> result;
+    for (int i  = 0;)
+}
 
 int main() {
 	vector<int> v = { 73, 74, 75, 71, 69, 72, 76, 73 };
